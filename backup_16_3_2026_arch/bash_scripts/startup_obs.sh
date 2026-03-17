ca34880adcc8c809f8168111de1ed282bf497c38
piadi-su <213212422+piadi-su@users.noreply.github.com>
2026-03-17 23:31:42 +0100

    Add files via upload

---
+++ backup_16_3_2026_arch/bash_scripts/startup_obs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# --- Avvia Pipewire ---
+systemctl --user enable --now pipewire pipewire-pulse pipewire-media-session
+
+# --- Avvia XDG Desktop Portal per WLR ---
+/usr/lib/xdg-desktop-portal-wlr &
+
+# --- Avvia Gnome Keyring (opzionale, per Brave e gestione password) ---
+/usr/bin/gnome-keyring-daemon --start --components=secrets &
+
+# req
+# sudo pacman -S pipewire pipewire-pulse pipewire-alsa gst-plugin-pipewire xdg-desktop-portal xdg-desktop-portal-wlr
